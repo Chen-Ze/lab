ff069890f1fda29e098378dcafbd5bc33ca0bc30
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-09-25 21:36:00 +0800

    call npm

--- install-windows.bat
+++ install-windows.bat
@@ -2,14 +2,14 @@ git clone https://github.com/Chen-Ze/python-visa-server
 
 git clone https://github.com/Chen-Ze/lab-server
 cd lab-server
-npm i
-npm run build
+call npm i
+call npm run build
 cd ..
 
 git clone https://github.com/Chen-Ze/lab-client
 cd lab-client
-npm i
-npm run build
+call npm i
+call npm run build
 cd ..
 
 pause
--- update-windows.bat
+++ update-windows.bat
@@ -5,12 +5,12 @@ cd ..
 cd lab-server
 git pull
 cd ..
-npm i
-npm run build
+call npm i
+call npm run build
 
 cd lab-client
 git pull
-npm i
-npm run build
+call npm i
+call npm run build
 
 pause
