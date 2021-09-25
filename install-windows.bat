a727037588f94a54b73261620182e7fdca56cd1f
陈泽 <chenze_ustc@mail.ustc.edu.cn>
2021-09-25 23:40:04 +0800

    zipjs.bat typo 2

--- install-windows.bat
+++ install-windows.bat
@@ -1,7 +1,7 @@
 curl -o python3.zip https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-amd64.zip
 
 git clone https://github.com/npocmaka/batch.scripts
-call .\batch.scripts\hybrids\jscript\zipjs.bat -source %cd%\python3.zip -destination %cd%\python3
+call .\batch.scripts\hybrids\jscript\zipjs.bat unzip -source %cd%\python3.zip -destination %cd%\python3
 
 git clone https://github.com/Chen-Ze/python-visa-server
 cd python3
